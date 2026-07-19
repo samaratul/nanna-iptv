4538af8320253d137742693ec6a8bce71defe37c
Atul Upadhyaya <samaratul@gmail.com>
2026-07-19 17:14:33 +0545

    Rename banner to ic_banner_v2 to bypass Android TV launcher cache, add banner to activity

--- tv-app/app/build.gradle.kts
+++ tv-app/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -26,10 +27,6 @@ android {
 
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
     }
 }
 
